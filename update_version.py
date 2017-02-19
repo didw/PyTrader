37ba7782780fe61597b4d70ba201912afebe7791
jyyang <yangjy0113@gmail.com>
2017-02-19 13:03:40 +0900

    change path

--- update_version.py
+++ update_version.py
@@ -6,7 +6,7 @@ import os
 
 # Account
 account = []
-with open("C:\\Users\\seoga\\PycharmProjects\\PyTrader\\account.txt", 'r') as f:
+with open("account.txt", 'r') as f:
     account = f.readlines()
 app = application.Application()
 app.start("C:/Kiwoom/KiwoomFlash2/khministarter.exe")
@@ -29,6 +29,15 @@ certForm.TypeKeys(account[2])
 loginBtn = dlg.Button0
 loginBtn.Click()
 
-time.sleep(50)
+# 업데이트가 완료될 때 까지 대기
+while True:
+    time.sleep(5)
+    with os.popen('tasklist /FI "IMAGENAME eq khmini.exe"') as f:
+        lines = f.readlines()
+        if len(lines) >= 3:
+            break
+
+# 번개2 종료
+time.sleep(30)
 os.system("taskkill /im khmini.exe")
 
