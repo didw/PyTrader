f4025ea07b1b279bdad67782ebada75e2122902d
Jongyeol Yang <yangjy0113@gmail.com>
2017-02-26 12:49:41 +0900

    bug fix

--- simple_regression.py
+++ simple_regression.py
@@ -85,12 +85,17 @@ class SimpleModel:
         res = 0
         score = 0
         assert(len(pred) == len(Y_test))
-        score += np.sqrt(np.reduce_mean((Y_test - pred)*(Y_test - pred)))
+        pred = np.array(pred).reshape(-1)
+        Y_test = np.array(Y_test).reshape(-1)
+        for i in range(len(pred)):
+            score += (float(pred[i]) - float(Y_test[i]))*(float(pred[i]) - float(Y_test[i]))
+        score = np.sqrt(score/len(pred))
         print("score: %f" % score)
         for idx in range(len(pred)):
-            buy_price = X_test[idx][1]
-            if pred[idx] > buy_price:
-                res += (Y_test[idx] - buy_price)
+            buy_price = float(X_test[idx][1])
+            if pred[idx] > buy_price*1.1:
+                res += (int(Y_test[idx]) - buy_price*1.005)*(100000/buy_price)
+                print("buy: %d, sell: %d" % (buy_price, int(Y_test[idx])))
         print("result: %d" % res)
 
 
